8c0a87d5d59c0c9580b5fe9bd6e3f317e091193a
adsoSchez <sanchezlds27@gmail.com>
2026-07-28 18:30:53 -0500

    ajuste en variable vite y en suta de flujo entre comunicacion

--- src/api.js
+++ src/api.js
@@ -1,7 +1,4 @@
-const API_BASE_URL = (import.meta.env.VITE_API_URL || 'https://tareaspremi.us-east-2.elasticbeanstalk.com')
-  .replace(/^http:\/\//, 'https://')
-  .replace(/\/$/, '');
-const API_URL = `${API_BASE_URL}/api/tareas`;
+const API_URL = '/api/tareas';
 
 async function request(path = '', options = {}) {
   const response = await fetch(`${API_URL}${path}`, {
